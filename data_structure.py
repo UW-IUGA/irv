f98d92abbdb7567d2db11efac5a8e8350ad3cb74
Brendan Kellogg <brendan6@uw.edu>
2018-02-28 22:35:10 -0800

    structure updated for 2018 elections

--- data_structure.py
+++ data_structure.py
@@ -1,7 +1,16 @@
 CANDIDATE_ROW = 1
 FIRST_CANDIDATE_COL = 8
 FIRST_BALLOT_ROW = 4
-POSITIONS = [3, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 3, 3, 2]
+POSITIONS = [
+        2,1,
+        2,1,
+        1,1,
+        1,2,
+        2,1,
+        3,4,
+        3,2,
+        1,
+        2]
 POSITION_NAMES = [
     "IUGA President",
     "Winfo President",
@@ -17,5 +26,6 @@ POSITION_NAMES = [
     "Winfo Director of Diversity Efforts",
     "IUGA Creative Director",
     "Winfo Creative Director",
-    "IUGA Director of IT"
+    "IUGA Director of Information Technology",
+    "IUGA Director of Academic Support"
 ]
